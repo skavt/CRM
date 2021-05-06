c4c717c7bbeafed820781df477d192cbffaca0af
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2021-05-06 18:38:20 +0400

    Add portal url for mail

--- mail/reset_password.php
+++ mail/reset_password.php
@@ -4,6 +4,7 @@ use app\models\User;
 use yii\helpers\Html;
 
 /* @var $user User */
+$portalUrl = env('PORTAL_HOST')
 ?>
 
 <p><?php echo Yii::t('app', 'Hello') ?> <?= $user->username ?>,</p>
@@ -11,6 +12,6 @@ use yii\helpers\Html;
 <p>
     <?php echo Yii::t('app', 'Follow the link below to reset your password') ?>
 	<br>
-    <?php echo Html::a(Html::encode(Yii::t('app', 'Password Reset'))) ?>
+    <?php echo Html::a(Html::encode(Yii::t('app', 'Password Reset')), "$portalUrl/auth/password-reset/$user->password_reset_token") ?>
 </p>
 
