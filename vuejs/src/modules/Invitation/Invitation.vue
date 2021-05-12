562286158d7721bb0407c8d1783803d2cbe72a26
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2021-05-12 14:13:53 +0400

    Create invitation fields

--- vuejs/src/modules/Invitation/Invitation.vue
+++ vuejs/src/modules/Invitation/Invitation.vue
@@ -1,10 +1,26 @@
 <template>
-  <h4>Working</h4>
+  <UsersTable :fields="fields"/>
 </template>
 
 <script>
+import UsersTable from "./components/UsersTable";
+
 export default {
-  name: "Invitation"
+  name: "Invitation",
+  components: {UsersTable},
+  data() {
+    return {
+      fields: [
+        {key: 'id', label: 'ID', sortable: true},
+        {key: 'email', label: 'Email', sortable: true},
+        {key: 'statusLabel', label: 'Status', sortable: true},
+        {key: 'created_at', label: 'Invitation Date', sortable: true},
+        {key: 'created_by', label: 'Invited By', sortable: true},
+        {key: 'token_used_date', label: 'Registration Date', sortable: true},
+        {key: 'actions', label: 'Actions'},
+      ],
+    }
+  },
 }
 </script>
 
---
+++ vuejs/src/modules/Invitation/components/UsersTable.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-table :fields="fields" dark responsive="sm" small>
+
+  </b-table>
+</template>
+
+<script>
+export default {
+  name: "UsersTable",
+  props: {
+    fields: {
+      type: Array,
+      require: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+
+.table-responsive-sm {
+  padding-right: 8rem;
+  padding-left: 8rem;
+
+  > table {
+    > thead {
+      > tr {
+        > th {
+          white-space: nowrap;
+
+          &:focus {
+            outline: none;
+          }
+        }
+      }
+    }
+  }
+}
+
+</style>
