c9c54b0aa846e461de9036a13399e49009d46977
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2021-05-19 13:02:27 +0400

    Implement navbar

--- vuejs/src/core/components/layout/DefaultLayout.vue
+++ vuejs/src/core/components/layout/DefaultLayout.vue
@@ -1,13 +1,42 @@
 <template>
-  <router-view/>
+  <div id="menu-content-wrapper">
+    <navbar/>
+    <div id="content" class="mt-3">
+      <router-view/>
+    </div>
+  </div>
 </template>
 
 <script>
+import Navbar from "../navbar/Navbar";
+
 export default {
-  name: "DefaultLayout"
+  name: "DefaultLayout",
+  components: {Navbar},
 }
 </script>
 
-<style scoped>
+<style lang="scss">
 
+#app {
+  height: 100%;
+  display: flex;
+  overflow: hidden;
+  margin-top: 0;
+}
+
+#menu-content-wrapper {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+
+#content {
+  flex: 1;
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+}
 </style>
---
+++ vuejs/src/core/components/navbar/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-brand>Customer Relationship Management</b-navbar-brand>
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item>
+        <i class="fab fa-facebook-messenger"/>
+      </b-nav-item>
+      <b-nav-item class="logout" @click="onLogoutClick">
+        Logout
+        <i class="fas fa-sign-out-alt"/>
+      </b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+import authService from "../../services/authService";
+
+export default {
+  name: "Navbar",
+  methods: {
+    onLogoutClick() {
+      authService.logout()
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+.navbar {
+  padding: 15px;
+
+  > .navbar-nav {
+    > .nav-item {
+      > .nav-link {
+        color: white;
+        font-size: 14px;
+      }
+
+      > .nav-link:hover {
+        color: grey;
+        font-size: 14px;
+      }
+    }
+  }
+}
+
+</style>
