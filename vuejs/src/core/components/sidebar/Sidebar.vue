a8220620b0736583693622174739ac78a289579c
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2021-05-20 13:13:17 +0400

    Add sidebar component

---
+++ vuejs/src/core/components/sidebar/Sidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="menu">
+    <div class="menu-collapse-line">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar"
+}
+</script>
+
+<style scoped>
+
+</style>
