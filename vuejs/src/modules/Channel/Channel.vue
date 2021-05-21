4de1daea816331c369461413b46e860431d24dc7
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2021-05-21 12:56:55 +0400

    Implement channel card example

--- vuejs/src/modules/Channel/Channel.vue
+++ vuejs/src/modules/Channel/Channel.vue
@@ -1,21 +1,31 @@
 <template>
-  <b-card no-body class="invitation-card">
-    <b-card-header>
-      <b-button class="float-right" variant="secondary" size="sm" @click="onAddChannelClick">
-        <i class="fas fa-plus"/>
-        Add Channel
-      </b-button>
-    </b-card-header>
-
-  </b-card>
+  <div class="channel-card">
+    <b-card no-body>
+      <b-card-header>
+        <b-button class="float-right" variant="secondary" size="sm" @click="onAddChannelClick">
+          <i class="fas fa-plus"/>
+          Add Channel
+        </b-button>
+      </b-card-header>
+    </b-card>
+    <div class="page-content">
+      <div class="row ml-0">
+        <div class="card-wrapper mt-3 ml-3">
+          <channel-card/>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import {createNamespacedHelpers} from "vuex";
+import ChannelCard from "./components/ChannelCard";
 
 const {mapActions} = createNamespacedHelpers('channel')
 export default {
   name: "Channel",
+  components: {ChannelCard},
   methods: {
     ...mapActions(['setData']),
     onAddChannelClick() {
@@ -28,6 +38,16 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+.channel-card {
+  height: 5.6%;
+  width: 100%;
+  margin-top: 0 !important;
+}
+
+.page-content {
+  position: relative;
+}
 
 </style>
---
+++ vuejs/src/modules/Channel/components/ChannelCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <b-card class="card-table" no-body>
+    <b-card-body class="pb-0">
+      <b-card-title>
+        <i class="fas fa-layer-group"/>
+        Text
+      </b-card-title>
+      <b-card-text class="description">
+        Description Description Description Description Description Description
+      </b-card-text>
+    </b-card-body>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "ChannelCard"
+}
+</script>
+
+<style lang="scss" scoped>
+
+.card-table {
+  position: relative;
+  cursor: pointer;
+  transition: all 0.3s;
+  border-radius: 0;
+  height: 100%;
+  width: 300px;
+  min-height: 10rem;
+  min-width: 16rem;
+
+  &:hover {
+    box-shadow: 0 0 0 2px #003349;
+  }
+
+  .description {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+  }
+}
+</style>
