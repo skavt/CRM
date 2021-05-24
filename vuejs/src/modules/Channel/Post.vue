781851e365c64b244d7804f45e619ec31b108764
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2021-05-24 18:44:27 +0400

    Add button is channel view page too

--- vuejs/src/modules/Channel/Post.vue
+++ vuejs/src/modules/Channel/Post.vue
@@ -10,6 +10,14 @@
           <i class="fas fa-plus"/>
           Create Post
         </b-button>
+        <b-button class="mr-1" variant="outline-secondary" size="sm" @click="onChannelNewUserClick">
+          <i class="fas fa-plus"/>
+          Add User
+        </b-button>
+        <b-button class="mr-1" variant="outline-primary" size="sm" @click="onChannelEditClick">
+          <i class="fas fa-pencil-alt"/>
+          Edit Channel
+        </b-button>
         <b-button variant="outline-danger" size="sm" @click="onChannelDeleteClick">
           <i class="fas fa-trash"/>
           Delete Channel
@@ -23,37 +31,54 @@
 
       </div>
     </b-card-body>
+    <channel-modal/>
+    <channel-user-modal/>
   </b-card>
 </template>
 
 <script>
 import ViewSpinner from "../../core/components/view-spinner/view-spinner";
 import {createNamespacedHelpers} from "vuex";
+import ChannelModal from "./modals/ChannelModal";
+import ChannelUserModal from "./modals/ChannelUserModal";
 
 const {mapState, mapActions} = createNamespacedHelpers('channel')
 export default {
   name: "Post",
-  components: {ViewSpinner},
+  components: {ChannelUserModal, ChannelModal, ViewSpinner},
   data() {
     return {
       loading: false,
+      channelData: {},
     }
   },
   computed: {
     ...mapState(['channel']),
   },
+  watch: {
+    async ['$route.params.channelId']() {
+      await this.getChannelData()
+      this.channelData = this.channel.data.find(ch => ch.id === parseInt(this.$route.params.channelId))
+    },
+  },
   methods: {
-    ...mapActions(['deleteChannel']),
+    ...mapActions(['deleteChannel', 'showChannelModal', 'showChannelUserModal', 'getChannelData', 'getActiveUsers']),
     onCreatePostClick() {
 
     },
+    async onChannelNewUserClick() {
+      await this.getActiveUsers(this.channelData.id)
+      this.showChannelUserModal(this.channelData)
+    },
+    onChannelEditClick() {
+      this.showChannelModal(this.channelData)
+    },
     async onChannelDeleteClick() {
-      let channel = this.channel.data.find(ch => ch.id === parseInt(this.$route.params.channelId))
-      const result = await this.$confirm(`Are you sure you want to delete ${channel.name} channel?`, `Deleting Channel...`)
+      const result = await this.$confirm(`Are you sure you want to delete ${this.channelData.name} channel?`, `Deleting Channel...`)
       if (result) {
-        const {success, body} = await this.deleteChannel(channel.id)
+        const {success, body} = await this.deleteChannel(this.channelData.id)
         if (success) {
-          this.$toast(`Channel ${channel.name} deleted successfully`)
+          this.$toast(`Channel ${this.channelData.name} deleted successfully`)
           this.$router.push({name: 'channel'})
         } else {
           this.$toast(body.message, 'danger')
@@ -61,8 +86,10 @@ export default {
       }
     },
   },
-  mounted() {
+  async mounted() {
     this.loading = true
+    await this.getChannelData()
+    this.channelData = this.channel.data.find(ch => ch.id === parseInt(this.$route.params.channelId))
     this.loading = false
   }
 }
